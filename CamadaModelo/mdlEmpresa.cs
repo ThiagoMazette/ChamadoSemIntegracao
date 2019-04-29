94fefba371b2607739c25ff70e31dfafd505ae4b
ThiagoMazette <44231071+ThiagoMazette@users.noreply.github.com>
2019-04-29 08:27:52 -0300

    add
    add

---
+++ CamadaModelo/mdlEmpresa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CamadaModelos
+{
+    public class mdlEmpresa
+    {
+        public string CNPJ { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Chamado { get; set; }
+        public string Abrir { get; set; }
+        public string ID { get; set; }
+        public string Resumo { get; set; }
+        public string Contato { get; set; }
+        public string Atendimento { get; set; }
+        public string FiltroAbrirChamado { get; set; }
+        public string FiltrotxtProcurar { get; set; }
+
+        public string txtEmpresaID { get; set; }
+        public string txtEmpresa { get; set; }
+        public string cbbTecnico { get; set; }
+        public string txtTelefone { get; set; }
+        public string txtContato { get; set; }
+        public string txtID { get; set; }
+
+    }
+}
---
+++ CamadaModelo/mdlTecnico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CamadaModelos
+{
+    public class mdlTecnico
+    {
+        public string Nome { get; set; }
+        public string Ramal { get; set; }
+    }
+}
